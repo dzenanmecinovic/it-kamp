216f8592449f12ec40114bb04cf29fdde4f89019
dzenanmecinovic <mecinovicd@gmail.com>
2022-10-27 20:59:58 +0200

    6.Cas

---
+++ JavaScript/6. Cas/domaci.js
@@ -0,0 +1,32 @@
+// 1.
+// const prviBroj = +prompt("Unesite prvi broj: ")
+// const drugiBroj = +prompt("Unesite drugi broj: ")
+
+// if (isNaN(prviBroj) || isNaN(drugiBroj)) {
+//     console.log("Unete vrednosti moraju biti brojevi.")
+// } else if (prviBroj === drugiBroj) {
+//     console.log("Povrsina kvadrata iznosi" + " " + (prviBroj * drugiBroj))
+// } else if (prviBroj !== drugiBroj) {
+//     console.log("Povrsina pravougaonika iznosi" + " " + (prviBroj * drugiBroj))
+// }
+
+// Za proveru da li je vrednost nekog broja NaN se koristi metoda:
+// isNaN(vrednost)
+
+// 2.
+// const x = +prompt("Unesite prvi broj:")
+// const y = +prompt("Unesite drugi broj:")
+
+// if (isNaN(x) || isNaN(y)) {
+//     console.log("Unete vrednosti moraju biti brojevi.")
+// } else if (y = 0) {
+//     console.log("Deljenje nulom nije moguce.")
+// } else {
+//     console.log("Kolicnik unetih brojeva je" + (x / y))
+// }
+
+// if (x > 0 && x < 5) {
+//     console.log("x je izmedju 0 i 5")
+// } else if (x > 0) {
+//     console.log("x > 0")
+//
