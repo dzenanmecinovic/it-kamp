95b07e3283683555e55a02c87799431a1ac2519d
dzenanmecinovic <mecinovicd@gmail.com>
2022-11-06 22:03:25 +0100

    homework template xd

---
+++ JavaScript/DOMACI ZADACI/Domaci Zadatak 3/app.js
@@ -0,0 +1,7 @@
+// Za domaci:
+
+// 1. Prebrojati koliko se puta u unetom strinu pojavljuje slovo "M" (i veliko i malo). Npr. za string 'Mama ima momu, dobija se rezultat 5.
+
+// 2. Prebrojati koliko ima znakova koji su cifre u unetom stringu.
+
+const broj1 = prompt("Unesite neku recenicu:")
