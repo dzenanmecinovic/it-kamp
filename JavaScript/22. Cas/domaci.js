b8d55e1d25b9cbe55f836ff2e4ffafbecb20b682
dzenanmecinovic <mecinovicd@gmail.com>
2022-12-11 21:21:35 +0100

    protectEmail function i 22.cas

--- JavaScript/22. Cas/domaci.js
+++ JavaScript/22. Cas/domaci.js
@@ -13,3 +13,27 @@ console.log(poslednjihN("Pera ima devojku", 5))
 let testing = "Recenica za testiranje"
 let testing2 = testing.slice(-10)
 console.log(testing2);
+
+
+
+// after-work
+function kombinovani (str1, str2) {
+
+    var a = str1.split("");
+  
+    var b = str2.split("");
+  
+    var spojeniString = '';
+  
+  
+    for(var i = 0; i < a.length || i < b.length; i++) {
+     if(i < a.length)
+       spojeniString +=  a[i];
+     if(i < b.length)
+       spojeniString +=  b[i];
+    }
+  return spojeniString;
+  
+  }
+  console.log(kombinovani('treci tekst','drugi string'));
+  console.log(kombinovani('ab','defffff'));
