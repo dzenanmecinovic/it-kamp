ad5e7ecfd7a5f1b72bfbe86554eaba2a4ff31a4e
dzenanmecinovic <mecinovicd@gmail.com>
2022-12-21 20:57:47 +0100

    fn

--- JavaScript/26. Cas/domaci.js
+++ JavaScript/26. Cas/domaci.js
@@ -35,18 +35,28 @@ function uOpsegu(prvi, drugi) {
         return `Prvi broj se nalazi u opsegu od 40 do 60, a drugi u opsegu od 70 do 100`
     } else if(prvi >= 70 && prvi <= 100 && drugi >= 40 && drugi <= 60) {
         return `Prvi broj se nalazi u opsegu od 70 do 100, a drugi u opsegu od 40 do 60`
-    } else if(prvi >= 40 && prvi <= 60 && drugi < 40 && drugi > 100) {
+    } else if(prvi >= 40 && prvi <= 60 && drugi < 40 || drugi > 100) {
         return `Prvi broj se nalazi u opsegu od 40 do 60, a drugi se ne nalazi ni u jednom opsegu.`
-    } else if(prvi < 40 && prvi > 100 && drugi >= 70 && drugi <= 100) {
+    } else if(prvi >= 70 && prvi <= 100 && drugi < 40 || drugi > 100) {
+        return `Prvi broj se nalazi u opsegu od 70 do 100, a drugi se ne nalazi ni u jednom opsegu.`
+    } else if(prvi < 40 || prvi > 100 && drugi >= 70 && drugi <= 100) {
         return `Prvi broj se ne nalazi u ni jednom opsegu, a drugi broj se nalazi u opsegu od 70 do 100.`
-    } else if(prvi < 40 && prvi > 100 && drugi >= 40 && drugi <= 60) {
+    } else if(prvi < 40 || prvi > 100 && drugi >= 40 && drugi <= 60) {
         return `Prvi broj se ne nalazi ni u jednom opsegu, a drugi se nalazi u opsegu od 40 do 60.`
     }
     else {
         return `Brojevi se ne nalaze ni u jednom od dva data opsega.`
     }
 }
-console.log(uOpsegu(20, 41))
+console.log(uOpsegu("asd"))
+console.log(uOpsegu(50, 55))
+console.log(uOpsegu(80, 85))
+console.log(uOpsegu(50, 80))
+console.log(uOpsegu(80, 50))
+console.log(uOpsegu(50, 20))
+console.log(uOpsegu(80, 20))
+console.log(uOpsegu(20, 80))
+console.log(uOpsegu(20, 55))
 //////// 3. /////////
 // 3 Write a JavaScript program to find the larger number from the two given positive integers, the two numbers are in the range 40..60 inclusive.
 function veciBroj(prvi, drugi) {
