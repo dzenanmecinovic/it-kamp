789bd4581ead19cba054063e2657374e8ff991be
dzenanmecinovic <mecinovicd@gmail.com>
2023-01-07 02:29:56 +0100

    middleChar

--- JavaScript/16. Cas/app.js
+++ JavaScript/16. Cas/app.js
@@ -2,7 +2,7 @@
 // Ima 2 argumenta.
 // Po defaultu replace() metoda menja samo prvo pronalazenje datog stringa.
 
-const recenica = "Danas smo podelili sertifikate za HTML, CSS i JavaScript."
+const recenica = "Danas smo podelili sertifikate za HTML, CSS i JavaScript.";
 
 let recenica2 = recenica.replace("Danas", "Juce");
 console.log(recenica2);
@@ -11,33 +11,33 @@ console.log(recenica2);
 
 // Za promenu svih pronalazenja datog stringa cemo koristiti Regular Expressions:
 // 1. g (global)
-recenica2 = recenica.replace(/s/g, "t")
-console.log(recenica2)
+recenica2 = recenica.replace(/s/g, "t");
+console.log(recenica2);
 
 // 2. i (insensitive)
-recenica2 = recenica.replace(/s/i, "A")
-console.log(recenica2)
-recenica2 = recenica.replace(/s/ig, "A")
-console.log(recenica2)
+recenica2 = recenica.replace(/s/i, "A");
+console.log(recenica2);
+recenica2 = recenica.replace(/s/gi, "A");
+console.log(recenica2);
 
 // toUpperCase() metoda za pretvaranje celog stringa u velika slova.
-console.log(recenica.toUpperCase())
+console.log(recenica.toUpperCase());
 
 // toLowerCase() metoda za pretvaranje celog stringa u velika slova.
-console.log(recenica.toLowerCase())
-console.log(recenica)
+console.log(recenica.toLowerCase());
+console.log(recenica);
 // console.log(recenica2.toUpperCase())
 
 // concat() metoda vrsi spajanje dva ili vise stringova.
 
-let a = "Danas je 16. cas JavaScripta."
-let b = "Bice JavaScripta i preko 50 casova."
+let a = "Danas je 16. cas JavaScripta.";
+let b = "Bice JavaScripta i preko 50 casova.";
 // Varijanta bez concat()
-let c = a + " " + b
+let c = a + " " + b;
 console.log(c);
 
-c = a.concat(" ",b, "\nNakon toga cemo raditi React biblioteku." );
-console.log(c)
+c = a.concat(" ", b, "\nNakon toga cemo raditi React biblioteku.");
+console.log(c);
 
 // Napomena.
 // Sve string metode prave onvu promenljivu (jer su stringovi immutable vrednosti).
@@ -49,26 +49,29 @@ console.log(c)
 // Funkcija treba vratiti poruku:
 // argument + {njena duzina} + "nedovoljno za dalje ispitivanje".
 
-const mojaFunkcijaa = function(recenica) {
-    if(recenica.length > 9) {
-        let Pola;
-        if(recenica.length % 2 === 0) {
-            Pola = recenica.length / 2
-        } else if(recenica.length % 2 !== 0) {
-            Pola = Math.round(recenica.length / 2)
-        }
-        const polovina = recenica(Pola, recenica.length)
-        const izmena = polovina.replace(/a/g, "B")
-        const konacna = "Ovo je novodobijena recenica".concat(" ", izmena)
-        return konacna;
-    } else {
-        const recenica2 = recenica.concat(recenica.length, " ", "nedovoljno za dalje ispitivanje.")
-        return recenica2;
+const mojaFunkcijaa = function (recenica) {
+  if (recenica.length > 9) {
+    let Pola;
+    if (recenica.length % 2 === 0) {
+      Pola = recenica.length / 2;
+    } else if (recenica.length % 2 !== 0) {
+      Pola = Math.round(recenica.length / 2);
     }
-}
-
-console.log(mojaFunkcijaa("Kraj casa"))
-console.log(mojaFunkcijaa("Kraj casaaaaaaaaaaaaaaaaaaaaaaa"))
+    const polovina = recenica.slice(Pola, recenica.length);
+    const izmena = polovina.replace(/a/g, "B");
+    const konacna = "Ovo je novodobijena recenica".concat(" ", izmena);
+    return konacna;
+  } else {
+    const recenica2 = recenica.concat(
+      recenica.length,
+      " ",
+      "nedovoljno za dalje ispitivanje."
+    );
+    return recenica2;
+  }
+};
+console.log(mojaFunkcijaa("Kraj casa"));
+console.log(mojaFunkcijaa("Kraj casaaaaaaaaaaaaaaaaaaaaaaa"));
 
 // Domaci zadatak.
 // Napraviti funkciju koja trazi od korisnika da unese neku recenicu, zatim funkcija vraca,
--- JavaScript/VEZBANJE/appindex/codeWars.js
+++ JavaScript/VEZBANJE/appindex/codeWars.js
@@ -6,27 +6,126 @@
 // spinWords( "This is a test") => returns "This is a test"
 // spinWords( "This is another test" )=> returns "This is rehtona test"
 
-function spinWords(string) {
-  let reci = string.split(" ");
-  for (let i = 0; i < reci.length; i++) {
-    if (reci[i].length >= 5) {
-      reci[i] = reci[i].split("").reverse().join("");
+// function spinWords(string) {
+//   let reci = string.split(" ");
+//   for (let i = 0; i < reci.length; i++) {
+//     if (reci[i].length >= 5) {
+//       reci[i] = reci[i].split("").reverse().join("");
+//     }
+//   }
+//   return reci.join(" ");
+// }
+// console.log(spinWords("Hey fellow warriors"));
+
+//////////////////////////////////// like system ////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+/////////////////////////////////////////////////////////////////////////////////////////
+// function likes(names) {
+//   for (let i = 0; i < names.length; i++) {
+//     if (names[i] === 0) {
+//       return `No one likes this`;
+//     } else if (names[i] === 1) {
+//       return `${names[0]} likes this post.`;
+//     } else if (names[i] === 2) {
+//       return `${names[0]} i ${names[1]} like this post.`;
+//     } else if (names[i] === 3) {
+//       return `${names[0]}, ${names[1]}, ${names[2]} like this post.`;
+//     } else if (names[i] === 4) {
+//       return `${names[0]}, ${names[1]} and 2 others like this post.`;
+//     }
+//   }
+// }
+// console.log(likes([]));
+
+function likes(names) {
+  if (names.length == 0) {
+    return "no one likes this";
+  } else if (names.length == 1) {
+    return names[0] + " likes this";
+  } else if (names.length == 2) {
+    return names[0] + " and " + names[1] + " like this";
+  } else if (names.length == 3) {
+    return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+  } else {
+    return (
+      names[0] +
+      ", " +
+      names[1] +
+      " and " +
+      (names.length - 2) +
+      " others like this"
+    );
+  }
+}
+
+// console.log(likes(["Prvi", "drugi"]));
+//// if amount of X and O is the same - retur true - else false; ////////////
+function XO(str) {
+  let x = 0;
+  let o = 0;
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === "x" || str[i] === "X") {
+      x++;
+    } else if (str[i] === "o" || str[i] === "O") {
+      o++;
     }
   }
-  return reci.join(" ");
-}
-console.log(spinWords("Hey fellow warriors"));
-
-var isSquare = function (n) {
-  if (Math.ceil(Math.sqrt(n)) === Math.floor(Math.sqrt(n))) {
+  if (x == 0 && o == 0) {
+    return true;
+  } else if (x == o) {
     return true;
   } else {
     return false;
   }
-};
-// console.log(Math.floor(Math.sqrt(9)));
-// console.log(Math.ceil(Math.sqrt(9)));
-// console.log(Math.floor(4.2));
-// console.log(Math.ceil(4.2));
+}
 
-console.log(isSquare(5));
+// console.log(XO("xxoxo", "o"));
+
+// function XOsolutions(string) {
+//   return (
+//     string.toLowerCase().split("x").length ===
+//     string.toLowerCase().split("o").length
+//   );
+// }
+
+/////// find the middle character ////////
+// function getMiddle(s) {
+//   Middle = s.length / 2;
+//   if (s.length % 2 !== 0) {
+//     return `Neparan`;
+//   } else {
+//     return Middle;
+//   }
+// }
+
+// console.log(getMiddle("tris"));
+
+function extractMiddle(s) {
+  var position;
+  var length;
+
+  if (s.length % 2 !== 0) {
+    position = s.length / 2;
+    length = 1;
+  } else {
+    position = s.length / 2 - 1;
+    length = 2;
+  }
+
+  return `${s.substr(position, length)}`;
+}
+
+console.log(extractMiddle("Sredin"));
+// console.log(extractMiddle("Sred"));
+// console.log(extractMiddle("Sred"));
+/////////////////////////////////////////////////////////////////////////////////////////
+var string = "Sredin";
+var position = "Sredin".length / 2;
+var length = 1;
+console.log(position);
+console.log(string.substr(position, length));
