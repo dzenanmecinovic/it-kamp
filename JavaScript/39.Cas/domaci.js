bbf0b859190aa4ed7f44e5794f4ce14ecfac1280
dzenanmecinovic <mecinovicd@gmail.com>
2023-01-22 01:50:54 +0100

    remote

---
+++ JavaScript/39.Cas/domaci.js
@@ -0,0 +1,5 @@
+// Na osnovu stringa koji unosi korisnik napraviti niz, gde cemo ispitati da li se jagoda nalazi unutar tog niza. Ako se jagoda ne nalazi unutar niza, funkcija neka vrati taj niz poredjan abecedno, s tim sto ce se na prvom mestu dodati broj 10, a na poslednjem broj 100.
+// Dok ako se jagoda nalazi unutar niza vratiti poziciju gde se ona nalazi.
+"use strict";
+function jagoda() {}
+console.log(jagoda());
