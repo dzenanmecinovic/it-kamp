f785bee9c928b304aab25dcdda42237d3d99b232
dzenanmecinovic <mecinovicd@gmail.com>
2023-02-22 19:42:42 +0100

    ¿

--- HTML-CSS/VEZBANJE/myPortfolio/app.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelectorAll(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
