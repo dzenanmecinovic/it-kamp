5292238bdaab5cb4a9a40b113fe685fc01f879cd
dzenanmecinovic <mecinovicd@gmail.com>
2023-02-22 20:11:27 +0100

    primenum

--- JavaScript/VEZBANJE/appindex/app.js
+++ JavaScript/VEZBANJE/appindex/app.js
@@ -1209,3 +1209,17 @@
 // console.log(numberOfDaysInMonth(1, 2012));
 // console.log(numberOfDaysInMonth(2, 2013));
 // console.log(numberOfDaysInMonth(2, 2008));
+
+// Prime num
+
+// const primeNum = (num) => {
+//   for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+//     if (num % i === 0) return false;
+//   }
+//   return num > 1;
+// };
+
+// console.log(primeNum(7));
+// console.log(primeNum(4));
+// console.log(primeNum(3));
+// console.log(primeNum(2));
